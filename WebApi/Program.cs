4aba608c2c5817ba1dd853f1715df373debd7859
Carthur83 <christian_A_83@hotmail.com>
2025-05-28 13:16:13 +0200

    Added GrpcEventService

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -8,12 +8,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+builder.Services.AddGrpc();
+
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("EventsDb")));
 builder.Services.AddScoped<IEventRepository, EventRepository>();
 builder.Services.AddScoped<IPackageRepository, PackageRepository>();
 builder.Services.AddScoped<IEventService, EventService>();
 
 var app = builder.Build();
+
+app.MapGrpcService<GrpcEventService>();
+
 app.MapOpenApi();
 app.UseHttpsRedirection();
 
---
+++ WebApi/Services/GrpcEventService.cs
@@ -0,0 +1,32 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+
+namespace WebApi.Services;
+
+public class GrpcEventService(IEventService eventService) : EventContract.EventContractBase
+{
+    private readonly IEventService _eventService = eventService;
+
+    public override async Task<EventReply> GetEventWithPackageById(EventByIdRequest request, ServerCallContext context)
+    {
+        var result = await _eventService.GetEventAsync(x => x.Id == request.EventId);
+        if (result.Result == null)
+        {
+            throw new RpcException(new Status(StatusCode.NotFound, "Event not found"));
+        }
+
+        var package = result.Result.Packages.FirstOrDefault(x => x.Id == request.PackageId);
+        if (package == null)
+        {
+            throw new RpcException(new Status(StatusCode.NotFound, "Package not found"));
+        }
+
+        return new EventReply
+        {
+            EventName = result.Result.Title,
+            PackageName = package.Title,
+            EventDate = Timestamp.FromDateTime(result.Result.EventDate.ToUniversalTime()),
+            Location = result.Result.Location
+        };
+    }
+}
